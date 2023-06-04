3d24ef32c33d9a1f75123bee11e453eaf70c8da5
missgoin <mister.goindo@gmail.com>
2023-06-04 17:33:43 +0700

    magriban dulu

--- .circleci/vince.sh
+++ .circleci/vince.sh
@@ -48,7 +48,7 @@ FINAL_ZIP_ALIAS=Karenulvin-${TANGGAL}.zip
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=cosmic
+COMPILER=proton
 
 ##----------------------------------------------------------##
 # Specify Linker
